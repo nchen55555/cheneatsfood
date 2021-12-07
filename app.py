f8c99b119968f1d52beed974a2795ad159753a99
Nicole Chen <nchen55555@gmail.com>
2021-12-07 17:50:44 -0500

    changes

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 #connects to the camps database
-db = sqlite3.connect("camps.db", check_same_thread=False)
+db = sqlite3.connect("camps.db", check_same_thread = False)
 #creates a table called volunterResponses under the camps database
 db.execute("CREATE TABLE IF NOT EXISTS 'volunteerResponses' ('volunteer_id' INTEGER NOT NULL UNIQUE PRIMARY KEY, 'first' TEXT NOT NULL, 'last' TEXT NOT NULL, 'age' INTEGER NOT NULL, 'email' TEXT NOT NULL, 'interest' TEXT NOT NULL, 'experience' TEXT NOT NULL, 'notes' TEXT NOT NULL)")    
 #creates a table called registration under the camps database 
@@ -71,7 +71,7 @@ def about():
     return render_template("about.html")
 
 #sessions and information page 
-@app.route("/info", methods=["GET", "POST"])
+@app.route("/info", methods = ["GET", "POST"])
 def info():
     """Sessions and Information"""
     #sets the color variable to red (will display message in red css)
@@ -82,14 +82,13 @@ def info():
  
     #request is through POST
     else:  
-
         #checks to see if the inputted age is a valid integer  
         try:
             int(request.form.get("age"))
         except ValueError:
             #outputs an error message if the inputted age is invalid 
             message = "Input a Valid Age Please"
-            return render_template("info.html", message=message, color=color)
+            return render_template("info.html", message = message, color = color)
 
         #collects first name, last name, age, email, phone number, and camp selection using request from the html form
         first = request.form.get("firstname")
@@ -105,22 +104,22 @@ def info():
         #checks to see if user inputted required fields 
         if not first:
             message = "Please input a first name"
-            return render_template("info.html", message=message, color=color)
+            return render_template("info.html", message = message, color = color)
         if not last:
             message = "Please input a last name"
-            return render_template("info.html", message=message, color=color)
+            return render_template("info.html", message = message, color = color)
         if not age:
             message = "Please input an age"
-            return render_template("info.html", message=message, color=color)
-        if email:
+            return render_template("info.html", message = message, color = color)
+        if not email:
             message = "Please input an email"
-            return render_template("info.html", message=message, color=color)
+            return render_template("info.html", message = message, color = color)
         if not number:
             message = "Please input a phonenumber"
-            return render_template("info.html", message=message, color=color)
+            return render_template("info.html", message = message, color = color)
         if not camp1 and not camp2:
             message = "Please select a camp session"
-            return render_template("info.html", message=message, color=color)
+            return render_template("info.html", message = message, color = color)
 
         #documents the camps signed up using integers - if the user signed up for both camps
         #camps will be 3. If the user signed up for session 2, camps will be 2. 
@@ -161,7 +160,7 @@ def info():
                     break
                 
                 #however if there are others in registrant with the same last and first name, then index increments and is added on to name 
-                index+=1
+                index += 1
                 #by adding on the new index to first to create name, the inserted username value will be different from registrants with the same first and last name
                 name = first + str(index)
                 
@@ -184,10 +183,10 @@ def info():
             message = "There was a problem with your email. Please enter a valid email address"
 
         #returns the info.html page with the constructed message and specified color
-        return render_template("info.html", message=message, color=color)
+        return render_template("info.html", message = message, color = color)
 
 #contact page
-@app.route("/contact", methods=["GET", "POST"])
+@app.route("/contact", methods = ["GET", "POST"])
 def contact():
     """Show contact page"""
     #sets the color variable to red (will display message in red css)
@@ -206,13 +205,13 @@ def contact():
         #checks to see if the user inputted a name, email, and message
         if name is None:
             m = "Please input a name"
-            return render_template("contact.html", m=m, color=color)
+            return render_template("contact.html", m = m, color = color)
         if email is None:
             m = "Please input an email"
-            return render_template("contact.html", m=m, color=color)
+            return render_template("contact.html", m = m, color = color)
         if message is None:
             m = "Please input a message"
-            return render_template("contact.html", m=m, color=color)
+            return render_template("contact.html", m = m, color = color)
 
         #validates the email input
         response = requests.get(
@@ -231,12 +230,11 @@ def contact():
             msg = Message('Contact Us Message', sender = 'dtccscamp@gmail.com', recipients = ['dtccscamp@gmail.com'])
             msg.body = name + " with an email of " + email + " sent you this message: \n" + message
             mail.send(msg)
-        
         #if the email inputted is not valid
         else:
             m = "There was a problem with your email. Please input a valid email."
 
-        return render_template("contact.html", m=m, color=color)
+        return render_template("contact.html", m = m, color = color)
 
 #blog page
 @app.route("/blog")
@@ -246,7 +244,7 @@ def blog():
     #note: imagesource is the image file name included in the blog post (ie. nicole.jpg)
     blogs = db.execute("SELECT title, blog_id, date, imagesource, text FROM blogs").fetchall()
     #returns blogs to the blog.html so that the html page can access the database information 
-    return render_template("blog.html", blogs=blogs)
+    return render_template("blog.html", blogs = blogs)
 
 #subblog page (aka. when users click a blog post to read, they come to the subblog page)
 @app.route("/subblog")
@@ -270,10 +268,10 @@ def subblog():
     #by the database. 
     #Note: the Markup function by text ensures that the text added to the database contains and utilizes HTML 
     #ex. there are parts in text that have html tags so that the webpage knows where to indent and add a new line
-    return render_template("subblog.html", title=title,date=date,text=Markup(text),image=image)
+    return render_template("subblog.html", title = title,date = date,text = Markup(text),image = image)
 
 #resources page that is only accessible if the user is logged in
-@app.route("/resources", methods=["GET", "POST"])
+@app.route("/resources", methods = ["GET", "POST"])
 @login_required
 def resources():
     """Show resources page"""
@@ -284,7 +282,7 @@ def resources():
     
     #checks to see if the request was through GET or POST
     if request.method == "GET":
-        return render_template("resources.html", admin=admin)
+        return render_template("resources.html", admin = admin)
     #the request was sent through POST
     else:
         #gets the title, date, image, and text from the blog post form from resources.html
@@ -299,21 +297,21 @@ def resources():
         #checks to see if user inputted a title, date, image source, and text or else outputs an error message
         if not title:
             message = "Please input a title."
-            return render_template("resources.html", color=color, message=message)
+            return render_template("resources.html", color = color, message = message)
         if not date:
             message = "Please input a date."
-            return render_template("resources.html", color=color, message=message)
+            return render_template("resources.html", color = color, message = message)
         if not image:
             message = "Please input an image source"
-            return render_template("resources.html", color=color, message=message)
+            return render_template("resources.html", color = color, message = message)
         if not text:
             message = "Please input blog text"
-            return render_template("resources.html", color=color, message=message)
+            return render_template("resources.html", color = color, message = message)
 
         #checks to see if the user is an admin or not to make sure that blog posts are only sent by administrators
         if not admin:
             message = "You are not an administrator and therefore do not have access to post blogs."
-            return render_template("resources.html", color=color, message=message)
+            return render_template("resources.html", color = color, message = message)
         
         #tries to insert blogs according to the inputted data
         try:
@@ -326,7 +324,7 @@ def resources():
         return redirect("/blog")
 
 #password change page that requires the user to be logged in
-@app.route("/change", methods=["GET", "POST"])
+@app.route("/change", methods = ["GET", "POST"])
 @login_required
 def change():
     """Show register page"""
@@ -348,13 +346,13 @@ def change():
         #checks to see if the user inputted an oldusername, oldpassword, and newpassword
         if not oldusername:
             message = "Please input your old username. If you forgot your username, you will have to email us to reset it for extra security purposes."
-            return render_template("change.html", color=color, message=message)
+            return render_template("change.html", color = color, message = message)
         if not oldpassword:
             message = "Please input your old password. If you forgot your password, you will have to email us to reset it for extra security purposes."
-            return render_template("change.html", color=color, message=message)
+            return render_template("change.html", color = color, message = message)
         if not newpassword:
             message = "Please input your new password."
-            return render_template("change.html", color=color, message=message)
+            return render_template("change.html", color = color, message = message)
 
         #selects from the database all columns where the username is the oldusername and the member_id matches with the current signed in member_id
         rows = db.execute("SELECT * FROM members WHERE username = ? AND member_id = ?", (oldusername, session["user_id"][0])).fetchall()
@@ -375,11 +373,11 @@ def change():
         #or if there are no instances where the old password matches the stored database password
         else:
             #then output the message below in red
-            message="Username and/or Password Incorrect"
-            return render_template("change.html", message=message, color=color)
+            message = "Username and/or Password Incorrect"
+            return render_template("change.html", message = message, color = color)
 
 #login page
-@app.route("/login", methods=["GET", "POST"])
+@app.route("/login", methods = ["GET", "POST"])
 def login():
     """Show signin page"""
     #checks to see if the request was through GET or POST 
@@ -398,11 +396,11 @@ def login():
 
         #checks to see if the user inputted a username and password 
         if not username:
-            message="Please input an username"
-            return render_template("login.html", message=message, color=color)
+            message = "Please input an username"
+            return render_template("login.html", message = message, color = color)
         if not password:
-            message="Please input a password"
-            return render_template("login.html", message=message, color=color)
+            message = "Please input a password"
+            return render_template("login.html", message = message, color = color)
         
         #selects hash from members where the username is equal to the inputted username
         hash = db.execute("SELECT hash FROM members WHERE username = ?", [username]).fetchall()
@@ -410,12 +408,12 @@ def login():
         if hash == []:
             #no hash selected so the username was incorrect (there was no username)
             message="Username and/or Password Incorrect"
-            return render_template("login.html", message=message, color=color)
+            return render_template("login.html", message = message, color = color)
         #checks to see if the inputted password matches the stored hashed password
         if not check_password_hash(hash[0][0],password):
             #don't match so password was incorrect 
-            message="Username and/or Password Incorrect"
-            return render_template("login.html", message=message, color=color)
+            message = "Username and/or Password Incorrect"
+            return render_template("login.html", message = message, color = color)
         else:
             #selects id from members where the username is equal to the inputted username
             id = db.execute("SELECT member_id FROM members WHERE username = ?", [username]).fetchone()
@@ -424,45 +422,52 @@ def login():
             #redirects the user to the resources page 
             return redirect("/resources")
 
+#logout page
 @app.route("/logout")
 def logout():
     """Log user out"""
-
+    #clears session
     session.clear()
-
+    #redirects program to main page
     return redirect("/")
 
-
+#flyer page 
 @app.route("/flyer")
 def flyer():
-    """Flyer""" 
+    """Flyer"""
     return render_template("flyer.html")
 
+#mentors page 
 @app.route("/mentors")
 def mentors():
     """Mentors"""
     return render_template("mentors.html")
 
+#professionals page 
 @app.route("/professionals")
 def professionals():
     """Professionals"""
     return render_template("professionals.html")
 
-@app.route("/volunteers", methods=["GET", "POST"]) 
+#volunteers page
+@app.route("/volunteers", methods = ["GET", "POST"]) 
 def volunteers():
     """Volunteers"""
-    message = ""
-    color="red"
+    #makes the color css red
+    color = "red"
+    #checks to see if request was taken via GET or POST 
     if request.method == "GET":
-        return render_template("volunteers.html", message=message)
- 
+        return render_template("volunteers.html")
+    #request was taken via POST 
     else:
+        #tries to type cast the age element received from HTML as an integer 
         try:
             int(request.form.get("age"))
         except ValueError:
-            message="Please input a valid age"
-            return render_template("volunteers.html", message=message, color=color)
+            message = "Please input a valid age"
+            return render_template("volunteers.html", message = message, color = color)
 
+        #gets the first name, last name, age, email, interest, experience, and notes from the volunteers html 
         first = request.form.get("firstname")
         last = request.form.get("lastname")
         age = int(request.form.get("age"))
@@ -471,44 +476,49 @@ def volunteers():
         experience = request.form.get("experience")
         notes = request.form.get("notes")
 
+        #checks to see if the user inputted a firstname, lastname, age, email, interest, and experience
         if not first:
             message = "Please input first name."
-            return render_template("change.html", message=message, color=color)
+            return render_template("change.html", message = message, color = color)
         if not last:
             message = "Please input your last name."
-            return render_template("change.html", message=message, color=color)
+            return render_template("change.html", message = message, color = color)
         if not age:
             message = "Please input your age."
-            return render_template("change.html", message=message, color=color)
+            return render_template("change.html", message = message, color = color)
         if not email:
             message = "Please input your email."
-            return render_template("change.html", message=message, color=color)
+            return render_template("change.html", message = message, color = color)
         if not interest:
             message = "Please input your interest."
-            return render_template("change.html", message=message, color=color)
+            return render_template("change.html", message = message, color = color)
         if not experience:
             message = "Please input your experience."
-            return render_template("change.html", message=message, color=color)
+            return render_template("change.html", message = message, color = color)
         
+        #validates the email inputted 
         response = requests.get(
             "https://isitarealemail.com/api/email/validate",
         params = {'email': email})
 
         status = response.json()['status']
+        #checks to see if email is valid
         if status == "valid":
             message = "Thank you for submitting your application!"
+            #inserts the inputted firstname, lastname, age, email, interest, experience, and notes into the volunteerResponses table
             db.execute("INSERT INTO volunteerResponses (first, last, age, email, interest, experience, notes) VALUES (?, ?, ?, ?, ?, ?, ?)", 
                    (first, last, age, email, interest, experience, notes))
             db.commit()
+            #sends a mail message to the volunteer confirming their application
             msg = Message('DTC Computer Science Camp Volunteer Application', sender = 'dtccscamp@gmail.com', recipients = [email])
             msg.body = "Hi! Thank you for submitting your volunteer application to DTC Computer Science Camps! We will send out another email in the next coming weeks with more information on your application and its next steps."
             mail.send(msg)
             color = "green"
         else:
+            #if the email is not valid then send them this email 
             message = "There was a problem with your email. Please enter a valid email address"
-
-        return render_template("info.html", message=message, color=color)
+            return render_template("info.html", message = message, color = color)
         
-        return render_template("volunteers.html", message=message)
+        return render_template("volunteers.html", message = message, color = color)
 
 
