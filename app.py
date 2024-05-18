6efd0bd98a8e907fa479e16246f0f4b138a544ca
Nicole Chen <nicole_chen@college.harvard.edu>
2024-05-17 22:37:54 -0600

    requirements

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import sqlite3
-import requests
 
-from flask import Flask, render_template, request, url_for, flash, redirect, Markup
+from flask import Flask, render_template, request, flash, redirect, Markup
 from werkzeug.utils import secure_filename
 import os
 
